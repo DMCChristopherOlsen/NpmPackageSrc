a311f9fa0003218b259fe1df1567c32e9d2bb20c
DMC\christophero <christopher.olsen@dmcinfo.com>
2018-03-14 16:29:49 -0500

    - Added dist

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+exports.addTwoNumbers = addTwoNumbers;
